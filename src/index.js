d1adb8a1f676f07916bef600753ca1892575fa32
Olga Kalacheva <kalach2256954@gmail.com>
2023-01-07 20:59:09 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let res = String(n);
+    let arr = res.split("");
+    if (n > 0) {
+        let revArr = arr.reverse();
+        let num = Number(revArr.join(""));
+        return num;
+    } else {
+        let cut = arr.slice(1);
+        let revArr = cut.reverse();
+        let num = Number(revArr.join(""));
+        return num;
+    }
+};
